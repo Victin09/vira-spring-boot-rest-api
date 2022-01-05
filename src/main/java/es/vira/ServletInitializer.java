81f46f1a6e3c05dd338a5c7850746f5b8637909f
Victor Gomez <victor.gomez@ghenova.net>
2022-01-05 11:32:38 +0100

    add ServletInitializer class

---
+++ src/main/java/es/vira/ServletInitializer.java
@@ -0,0 +1,14 @@
+package es.vira;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ViraRestApiApplication.class);
+    }
+
+}
+
