e670a9a7c4342d0788e0a7df6e86789fff776b34
Victor Gomez <victor.gomez@ghenova.net>
2022-01-12 12:57:00 +0100

    update spring boot version to 2.6.2

--- src/main/java/es/vira/ViraRestApiApplication.java
+++ src/main/java/es/vira/ViraRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ViraRestApi {
+public class ViraRestApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ViraRestApi.class, args);
+        SpringApplication.run(ViraRestApiApplication.class, args);
     }
 
 }
